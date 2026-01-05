de7c33372a8209e3055c22933f24d6b81480d9bb
Adam Gamble <adam.gamble@hey.com>
2026-01-05 14:00:36 +0100

    Add a quick open-in-emacs command
    For quickly opening a file buffer from vterm. Got this idea from
    trying out Windsurf and having the terminal drive the current context.
    
    Still tbc. exactly the workflow here

--- home.nix
+++ home.nix
@@ -49,9 +49,10 @@
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "open-in-emacs" ''
+        FILE_TO_DISPLAY=''${1:-$PWD}
+        emacsclient -e "(display-buffer (find-file-noselect \"$FILE_TO_DISPLAY\"))"
+    '')
   ];
 
   programs.fish = {
